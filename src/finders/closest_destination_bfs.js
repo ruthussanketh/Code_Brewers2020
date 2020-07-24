166ec1102f42ba7eab894c9800911a146653d2cd
jagritiagarwal <48601324+jagritiagarwal@users.noreply.github.com>
2020-07-25 01:30:50 +0530

    Update closest_destination_bfs.js

--- src/finders/closest_destination_bfs.js
+++ src/finders/closest_destination_bfs.js
@@ -1,4 +1,5 @@
-	var util       = require('../core/Util');
+	var util  = require('../core/Util');
+	var DiagonalMovement = require('../core/DiagonalMovement');
         function closest_destination_bfs(t) {
           t = t || {
           },
@@ -7,35 +8,48 @@
         }
         
         closest_destination_bfs.prototype.findPath = function (startx, starty, endx, endy, grid) {
-          var p=[];
-          var c = this.allowDiagonal,
-          f = this.dontCrossCorners,
+          var openlist=[];
+          var diagonalMovement = this.allowDiagonal,
+         
           start= grid.getNodeAt(startx, starty),
           end = grid.getNodeAt(endx, endy);
           var stops=[]; //stops storing all destination points
          
-          
           stops.push(end);
          
-          
           for(var i=0;i<40;i++){
 
           for(var j=0;j<60;j++){
           if(grid.isNstopAt(j,i)==0){stops.push(grid.getNodeAt(j,i));}
           }
           }
-          for (p.push(start), start.opened = !0; p.length; ) {
-            if (u = p.shift(), u.closed = !0) {
-              for(var k=0;k<stops.length;k++){
-                
-                if(u===stops[k]){return util.backtrace(u);}}//return array having min path length from the start node
-              }
-              
-            for (s = grid.getNeighbors(u, c, f), h = 0, l = s.length; l > h; ++h) a = s[h],
-            a.closed || a.opened || (p.push(a), a.opened = !0, a.parent = u)//getting neighbours and stored the closed and opened values
-          }
-          return [];
-        };
+	  openList.push(start);
+	  start.opened=!0;
+	  while (openList.length) {
+		// take the front node from the queue
+		node = openList.shift();
+		node.closed = true;
+
+		// reached the end position
+		if (node === endNode) {
+		    return Util.backtrace(endNode);
+		}
+
+		neighbors = grid.getNeighbors(node, diagonalMovement);
+		for (i = 0, l = neighbors.length; i < l; ++i) {
+		    neighbor = neighbors[i];
+
+		    // skip this neighbor if it has been inspected before
+		    if (neighbor.closed || neighbor.opened) {
+			continue;
+		    }
+
+		    openList.push(neighbor);
+		    neighbor.opened = true;
+		    neighbor.parent = node;
+		}
+	    }
+         
         module.exports = closest_destination_bfs;
       
       
